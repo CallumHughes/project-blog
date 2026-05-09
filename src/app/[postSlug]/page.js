c923109778eed8ad1c0d7c0b6e44151a88062a7f
Callum Hughes <callumjhughes@gmail.com>
2026-05-09 18:12:48 +0100

    feat: task 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 import BlogHero from '@/components/BlogHero';
+import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import styles from './postSlug.module.css';
-import { loadBlogPost } from "@/helpers/file-helpers";
+
+
+export async function generateMetadata({ params }) {
+    const { postSlug } = await params;
+    const mdxBlogPost = await loadBlogPost(postSlug)
+
+    return {
+        title: mdxBlogPost.frontmatter.title,
+        description: mdxBlogPost.frontmatter.description,
+    };
+}
 
 
 async function BlogPost({ params }) {
-  const { postSlug } = await params;
-  const mdxBlogPost = await loadBlogPost(postSlug)
+    const { postSlug } = await params;
+    const mdxBlogPost = await loadBlogPost(postSlug)
 
   return (
     <article className={styles.wrapper}>
